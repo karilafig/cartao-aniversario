0c7006cbcfc9984b18e9b2b5b30eedcf29bd770f
karilaf <karinamalik2212@gmail.com>
2023-09-12 20:47:47 -0300

    feliz aniversario mozones

---
+++ script.js
@@ -0,0 +1,31 @@
+document.getElementById("open-card").addEventListener("click", function() {
+    var card = document.getElementById("card");
+    var arrowGif = document.querySelector(".arrow-gif");
+    var heartsContainer = document.querySelector(".hearts-container");
+
+    card.style.display = "block";
+    this.style.display = "none";
+    arrowGif.style.display = "none";
+    heartsContainer.style.display = "block";
+
+    // Adicionar corações com atraso
+    for (let i = 0; i < 10; i++) {
+        setTimeout(() => {
+            createHeart(heartsContainer);
+        }, i * 100); // Atraso de 100ms entre cada coração
+    }
+});
+
+function createHeart(container) {
+    const heart = document.createElement("div");
+    heart.className = "heart";
+    heart.style.left = Math.random() * 100 + "%";
+    const heartImage = document.createElement("img");
+    heartImage.src = "coracao.png"; // Substitua 'coracao.png' pela imagem do coração
+    heart.appendChild(heartImage);
+    container.appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    }, 5000);
+}
